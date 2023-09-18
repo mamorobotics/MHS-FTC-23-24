d042c63897f5d5913ff2806368cef37cdf4c73a3
Dabuilda <107159084+TheMagnificentMiles@users.noreply.github.com>
2023-09-18 08:39:40 -0400

    Rewipe && Update

---
+++ MeepMeep/src/main/java/com/example/meepmeep/red_close_to_spin.java
@@ -0,0 +1,36 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class red_close_to_spin {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(52.48180821614297, 52.48180821614297, Math.toRadians(184.02607784577722), Math.toRadians(184.02607784577722), 16.34)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, -62, Math.toRadians(90)))
+                                .forward(3)
+                                .lineToLinearHeading(new Pose2d(-55,-55,Math.toRadians(180)))
+                                .addDisplacementMarker(() -> {
+                                    //spin the thing
+                                })
+
+                                .lineToSplineHeading(new Pose2d(-24,-37,Math.toRadians(45)))                                .addTemporalMarker(()->{
+                                    //put the item to the right height
+                                })
+
+                                .lineToLinearHeading(new Pose2d(-60,-35,Math.toRadians(0)))
+                                .build()
+                        );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
