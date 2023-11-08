212a9dd2e9d6d972edb562420bd445013e287e56
maxjlaz1 <maxjlaz@icloud.com>
2023-11-08 10:30:54 -0500

    new track width added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1.0200972139; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.97; // in
+    public static double TRACK_WIDTH = 8.11024; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
