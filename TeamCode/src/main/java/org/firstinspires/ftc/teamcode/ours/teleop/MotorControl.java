7fb067db1a6a92856974fe797c9b58777481cb1c
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2023-11-09 15:29:19 -0500

    Initial Lesson 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ours/teleop/MotorControl.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.ours.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.*;
+
+public class OpModeTemplate extends OpMode {
+    //https://gm0.org/en/latest/docs/software/getting-started/linear-opmode-vs-opmode.html for more opmode functions
+    @Override
+    public void init() {
+        //Runs on press of init
+    }
+
+    @Override
+    public void start() {
+        //Runs once when start is pressed
+    }
+}
